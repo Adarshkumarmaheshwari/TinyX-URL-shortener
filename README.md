5321750718a16a2cb229d810ab9b7363cc1121bb
Adarshkumarmaheshwari <202012100@daiict.ac.in>
2021-05-13 01:04:58 +0530

    Added ss

---
+++ README.md
@@ -0,0 +1,12 @@
+# TinyX-URL-Shortner
+TinyX is a URL shortner which is based on node.js and express.js.\
+
+Below are few screenshots to give you a idea of website.
+### If there are no errors
+
+![No errors](https://raw.githubusercontent.com/Adarshkumarmaheshwari/TinyX-URL-shortner/master/public/green_ss.JPG)
+### URL does not exist (error)
+![if there are error](https://raw.githubusercontent.com/Adarshkumarmaheshwari/TinyX-URL-shortner/master/public/red_ss_url.JPG)
+
+### URL is not valid
+![if there are error](https://raw.githubusercontent.com/Adarshkumarmaheshwari/TinyX-URL-shortner/master/public/red_ss.JPG)
---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const shortId = require('shortid')
+const createHttpError = require('http-errors')
+const mongoose = require('mongoose')
+const path = require('path')
+const dotenv = require('dotenv')
+const { log } = require('console')
+const ShortUrl=require('./models/url.model')
+const connectDB = require('./config/db.env')
+
+
+const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+//Load config
+dotenv.config({path: './config/config.env'})
+
+connectDB()
+
+app.set('view engine', 'ejs')
+
+app.get('/', async(req, res, next) =>{
+    res.render('index')
+})
+
+app.post('/', async(req,res, next)=>{
+try{
+    const{url}= req.body
+    if(!url){
+        throw createHttpError.BadRequest('Kindly provide a valid URL!')
+    }
+    const urlExists = await ShortUrl.findOne({url})
+    if(urlExists){
+        res.render('index', {short_url: `${req.hostname}/${urlExists.shortId}`})
+        return
+    }
+    const shortUrl = new ShortUrl({url: url, shortId: shortId.generate()})
+    const result =await shortUrl.save()
+    res.render('index', {short_url: `${req.hostname}${result.shortId}`})
+}catch(error){
+    next(error)
+}
+})
+
+
+app.get('/:shortId', async (req, res, next) => {
+  try {
+    const { shortId } = req.params
+    const result = await ShortUrl.findOne({ shortId })
+    if (!result) {
+      throw createHttpError.NotFound('Url does not exist!')
+    }
+    res.redirect(result.url)
+  } catch (error) {
+    next(error)
+  }
+})
+//Error handling
+
+app.use((res, req, next)=>{
+    next(createHttpError.NotFound())
+})
+
+
+app.use((err, req, res, next)=>{
+    res.status(err.status || 500)
+    res.render('index', {error: err.message})
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(
+    PORT,
+    console.log(`Server running on port`)
+    )
